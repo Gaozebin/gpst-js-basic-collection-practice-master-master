bdb93f7fa84e44466f9f43863019d0643f483f41
Gaozebin <32197390+Gaozebin@users.noreply.github.com>
2017-11-05 20:32:05 +0800

    Update practice-3.js

--- gpst-js-basic-collection-practice-master-master/main/section-2/practice-3.js
+++ gpst-js-basic-collection-practice-master-master/main/section-2/practice-3.js
@@ -1,5 +1,62 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var A = [];
+	 var B = [];
+     for(var i = 0; i < collection.length;)
+     {
+         var count = 0;
+         var map = {};
+         for(var j = i; j < collection.length; j++)
+         {
+             if (collection[i] == collection[j])
+             {
+                 count++;
+             }
+         }
+         if(collection[i].indexOf('-') > 0){
+             map['key'] = collection[i].split("-")[0];
+             map['count'] = parseInt(collection[i].split("-")[1]);
+                        A.push(map);
+             i ++;
+             continue;
+         } else if(collection[i].indexOf('[') > 0){
+             map['key'] = collection[i].split("[")[0];
+             map['count'] = parseInt(collection[i].split("[")[1]);
+                        A.push(map);
+             i ++;
+             continue;
+         } else if(collection[i].indexOf(':') > 0){
+	         map['key'] = collection[i].split(":")[0];
+	         map['count'] = parseInt(collection[i].split(":")[1]);
+	                      A.push(map);
+	         i ++;
+	         continue;
+         }
+         map["key"] = collection [i];
+         map["count"] = count;
+                        A.push(map);
+         i += count;
+     }
+     
+     for(var i = 0;i < A.length; ){
+         var count = 0;
+         var count1=0;
+         var arr = {};
+         for(var j = i; j < A.length; j++)
+         {
+             if (A[i]['key'] == A[j]['key'])
+             {
+                 count++;
+                 count1 += A[j]['count'];
+             }
+         }
+         arr["name"] = A[i]['key'];
+         arr["summary"] = count1;
+         B.push(arr);
+         i += count;
+     }
+     return B;
+
 }
+
