0ffd62ef661f8bb31ecb1a41c9084e62caa485b5
Gaozebin <32197390+Gaozebin@users.noreply.github.com>
2017-11-05 18:56:03 +0800

    Update practice-1.js

--- gpst-js-basic-collection-practice-master-master/main/section-1/practice-1.js
+++ gpst-js-basic-collection-practice-master-master/main/section-1/practice-1.js
@@ -1,21 +1,17 @@
 'use strict';
 
 module.exports = function collectSameElements(collectionA, collectionB) {
-        var result = new Array();
-        var c=collectionB.toString();
+        var result = new[];
        for(var i=0;i<collectionA.length;i++)
              {
-               if(c.indexOf(collectionA[i].toString())>-1)
-               {
                  for(var j=0;j<collectionB.length;j++)
                  {
                      if(collectionA[i]==collectionB[j]) 
                      {
                         result.push(collectionA[i]);
-                        break;
+                             break;
                      }
                  }
               }      
-             }
              return result;
 }
