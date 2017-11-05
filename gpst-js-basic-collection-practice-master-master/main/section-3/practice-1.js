20ab23229e5b3c20f9d957f530c5f0ad16f19ae3
Gaozebin <32197390+Gaozebin@users.noreply.github.com>
2017-11-05 20:51:58 +0800

    Update practice-1.js

--- gpst-js-basic-collection-practice-master-master/main/section-3/practice-1.js
+++ gpst-js-basic-collection-practice-master-master/main/section-3/practice-1.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-var result =new Array（） ;
+var result =new Array() ;
 	for (var i = 0; i < collectionA.length; i++) {
         for (var j = 0; j < objectB.value.length; j++) {
             if (collectionA[i].key == objectB["value"][j]){
