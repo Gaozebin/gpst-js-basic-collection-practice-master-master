dd5f9c567f718c7024d4fcb48d8d43dfaada2b28
Gaozebin <32197390+Gaozebin@users.noreply.github.com>
2017-11-06 19:50:53 +0800

    Update practice-3.js

--- gpst-js-basic-collection-practice-master-master/main/section-3/practice-3.js
+++ gpst-js-basic-collection-practice-master-master/main/section-3/practice-3.js
@@ -1,26 +1,26 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
- var result = new Array();
-col.push({'key':'a','count':1});
-console.log(result);
-        for(var i = 1;i < collectionA.length;i++){
-                if(collectionA[i] != collectionA[i-1])
-                        result.push({'key':collectionA[i],'count':0});
-                for(var j = 0;j < result.length;j++) {
-                        if(collectionA[i] == result[j].key) {
-                                result[j].count++;
-                                break;
-                        }
-                }
+var collection = [];
+	for (var i = 0; i < collectionA.length; ) {
+		var count = 0;
+		var map ={};
+		for (var j = i; j < collectionA.length; j++) {
+			if(collectionA[i] == collectionA[j]){
+				count++;
+			}
+		}
+		map.key = collectionA[i];
+		map.count = count;
+		collection.push(map);
+		i += count;
+	}
+	for (var i = 0; i < collection.length; i++) {
+        for (var j = 0; j < objectB.value.length; j++) {
+            if (collection[i].key == objectB["value"][j]){
+            	 	collection[i].count -= parseInt(collection[i]['count']/3);
+        	}
         }
-	for(var i = 0;i < result.length;i++) {
-                for(var j = 0;j < objectB.value.length;j++) {
-                        if(result[i].key == objectB.value[j])
-                                result[i].count -= parseInt(result[i].count/3);
-                                console.log("-1s");
-                }
-        }
-        console.log(result);
-        return result;
-}	
+	}
+  	return collection;
+}
